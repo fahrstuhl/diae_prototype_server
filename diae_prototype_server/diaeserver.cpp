ebbe6f1249a3b89c941cae65cd4d3a65b7f76583
fahrstuhl <fahrstuhl@mailbox.tu-berlin.de>
2017-03-19 01:00:21 +0100

    adds server prototype as pretty much a copy of qtwebsockets-simplechat- chatserver example

---
+++ diae_prototype_server/diaeserver.cpp
@@ -0,0 +1,54 @@
+#include "diaeserver.h"
+#include "QtWebSockets/QWebSocketServer"
+#include "QtWebSockets/QWebSocket"
+
+QT_USE_NAMESPACE
+
+DiaeServer::DiaeServer(quint16 port, QObject *parent) :
+    QObject(parent),
+    m_pWebSocketServer(Q_NULLPTR)
+{
+    m_pWebSocketServer = new QWebSocketServer(QStringLiteral("DIAE Server"),
+                                              QWebSocketServer::NonSecureMode,
+                                              this);
+    if (m_pWebSocketServer->listen(QHostAddress::Any, port)) {
+        qDebug() << "DIAE server on port" << port;
+        connect(m_pWebSocketServer, &QWebSocketServer::newConnection,
+                this, &DiaeServer::onNewConnection);
+    }
+}
+
+DiaeServer::~DiaeServer()
+{
+    m_pWebSocketServer->close();
+    qDeleteAll(m_clients.begin(), m_clients.end());
+}
+
+void DiaeServer::onNewConnection()
+{
+    QWebSocket *pSocket = m_pWebSocketServer->nextPendingConnection();
+    connect(pSocket, &QWebSocket::textMessageReceived, this, &DiaeServer::processMessage);
+    connect(pSocket, &QWebSocket::disconnected, this, &DiaeServer::socketDisconnected);
+    m_clients << pSocket;
+}
+
+void DiaeServer::processMessage(QString message)
+{
+    QWebSocket *pSender = qobject_cast<QWebSocket *>(sender());
+    for (QWebSocket *pClient : qAsConst(m_clients)) {
+        if (pClient != pSender)
+        {
+            pClient->sendTextMessage(message);
+        }
+    }
+}
+
+void DiaeServer::socketDisconnected()
+{
+    QWebSocket *pClient = qobject_cast<QWebSocket *>(sender());
+    if (pClient)
+    {
+        m_clients.removeAll(pClient);
+        pClient->deleteLater();
+    }
+}
---
+++ diae_prototype_server/diaeserver.h
@@ -0,0 +1,16 @@
+#ifndef DIAESERVER_H
+#define DIAESERVER_H
+
+
+class diaeserver : public QObject
+{
+    Q_OBJECT
+public:
+    explicit diaeserver(QObject *parent = 0);
+
+signals:
+
+public slots:
+};
+
+#endif // DIAESERVER_H
